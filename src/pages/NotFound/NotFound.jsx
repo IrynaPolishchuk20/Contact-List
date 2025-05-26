a944c7eab98e912eddfff70df673a2677f4ffb51
unknown <irademianiuk@gmail.com>
2025-05-26 19:17:16 +0300

    Header

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router'
 
 export default function NotFound(){
     return(
-        <div className="notfound">
-            <div className="star star1"></div>
-            <div className="star star2"></div>
-            <div className="star star3"></div>
-            <div className="star star4"></div>
-            <h1 className="notfound-title">404</h1>
-            <p className="notfound-subtitle">Ой! Сторінку не знайдено 🌌</p>
-            <img src={astronaut} alt="Астронавт" className="notfound-image" />
-            <Link to="/" className="notfound-button">🚀 Назад на головну</Link>
+        <div classNameName="notfound">
+            <div classNameName="star star1"></div>
+            <div classNameName="star star2"></div>
+            <div classNameName="star star3"></div>
+            <div classNameName="star star4"></div>
+            <h1 classNameName="notfound-title">404</h1>
+            <p classNameName="notfound-subtitle">Ой! Сторінку не знайдено 🌌</p>
+            <img src={astronaut} alt="Астронавт" classNameName="notfound-image" />
+            <Link to="/" classNameName="notfound-button">🚀 Назад на головну</Link>
         </div>
     )
 }
