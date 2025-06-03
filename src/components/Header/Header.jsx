1b20c6182e064b9bd5443bc99856f64f4cdf9a79
unknown <irademianiuk@gmail.com>
2025-06-03 14:46:34 +0300

    header scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router";
+import './Header.scss'
 
 export default function Header() {
     return(
         <header className="container">
-            <div className="row">
+            <div className="row containerHeader">
                 <div className="col-12">
-                    <nav className="navbar bg-body-tertiary rounded shadow">
+                    <nav className="navbar">
                         <div className="container-fluid">
                            <div className="navbar-brand">
                                 <Link className="navbar-brand" to="/">
