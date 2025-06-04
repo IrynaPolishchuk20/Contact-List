1c4366c4d53929818f860cc313c5537f14770fbf
unknown <irademianiuk@gmail.com>
2025-06-04 21:20:58 +0300

    search in sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,9 @@
 import './Sidebar.scss'
 
 export default function Sidebar({stor}) {
-    const totalContacts = stor.contacts.length
+    const filteredContacts = stor.search  ? stor.contacts.filter(contact => `${contact.firstName} ${contact.lastName} ${contact.email} ${contact.phone}`.toLowerCase().includes(stor.search.toLowerCase()) ) : stor.contacts
+    
+    const totalContacts = filteredContacts.length
 
     const statusCounts={
         work: 0,
@@ -10,43 +12,43 @@ export default function Sidebar({stor}) {
         friends: 0,
         others:  0
     }
-   stor.contacts.forEach(contact => {
+   filteredContacts.forEach(contact => {
     statusCounts[contact.status] +=1
   });
  
    return(
         <aside className="container">
-        <div className="row containerSidebar">
-          <div className="col-12">
-            <div className="contacts-labels">
-              <div className="All mb-5 mt-4 d-flex justify-content-between">
-                <span>All contacts:</span><span>{totalContacts}</span>
-              </div>
-              <div className="list fs-5">
-                <div className="d-flex justify-content-between mb-3">
-                  <div>Work</div>
-                  <span>{statusCounts.work}</span>
+          <div className="row containerSidebar">
+            <div className="col-12">
+              <div className="contacts-labels">
+                <div className="All mb-5 mt-4 d-flex justify-content-between">
+                  <span>All contacts:</span><span>{totalContacts}</span>
                 </div>
-                <div className="d-flex justify-content-between mb-3">
-                  <div>Family</div>
-                  <span>{statusCounts.family}</span>
-                </div>
-                <div className="d-flex justify-content-between mb-3">
-                  <div>Friends</div>
-                  <span>{statusCounts.friends}</span>
-                </div>
-                <div className="d-flex justify-content-between mb-3">
-                  <div>Private</div>
-                  <span>{statusCounts.private}</span>
-                </div>
-                <div className="d-flex justify-content-between mb-3">
-                  <div>Others</div>
-                  <span>{statusCounts.others}</span>
+                <div className="list fs-5">
+                  <div className="d-flex justify-content-between mb-3">
+                    <div>Work</div>
+                    <span>{statusCounts.work}</span>
+                  </div>
+                  <div className="d-flex justify-content-between mb-3">
+                    <div>Family</div>
+                    <span>{statusCounts.family}</span>
+                  </div>
+                  <div className="d-flex justify-content-between mb-3">
+                    <div>Friends</div>
+                    <span>{statusCounts.friends}</span>
+                  </div>
+                  <div className="d-flex justify-content-between mb-3">
+                    <div>Private</div>
+                    <span>{statusCounts.private}</span>
+                  </div>
+                  <div className="d-flex justify-content-between mb-3">
+                    <div>Others</div>
+                    <span>{statusCounts.others}</span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
       </aside>
     )
 }
